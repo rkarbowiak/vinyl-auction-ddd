33162c843ad03c27f9a54864bb25fbe82ad96c14
Reevo55 <radoslaw.karbowiak@gorrion.io>
2025-05-22 16:41:37 +0200

    feat: auction

--- test/auction.test.ts
+++ test/auction.test.ts
@@ -1,9 +1,118 @@
-import { describe, it, expect } from "bun:test";
+import { describe, it, expect, beforeEach } from "bun:test";
+
+class Bid {
+  constructor(
+    private id: string,
+    private amount: number,
+    private bidderId: string,
+  ) {}
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getAmount(): number {
+    return this.amount;
+  }
+
+  public getBidderId(): string {
+    return this.bidderId;
+  }
+}
+
+class Auction {
+  private bids: Bid[] = [];
+
+  constructor(private ownerId: string, private endDate: Date = new Date()) {}
+
+  placeBid(bid: Bid) {
+    if (this.bids.length > 0 && bid.getAmount() <= this.getCurrentBid()) {
+      throw new Error("Bid amount must be higher than the current bid.");
+    }
+
+    if (this.endDate < new Date()) {
+      throw new Error("Auction has already ended.");
+    }
+
+    if (this.ownerId === bid.getBidderId()) {
+      throw new Error("Seller cannot bid on their own auction");
+    }
+
+    this.bids.push(bid);
+  }
+
+  getCurrentBid(): number {
+    return this.bids.length > 0
+      ? this.bids[this.bids.length - 1].getAmount()
+      : 0;
+  }
+
+  setOwnerId(ownerId: string) {
+    this.ownerId = ownerId;
+  }
+
+  getOwnerId(): string {
+    return this.ownerId;
+  }
+}
 
 describe("auction test", () => {
-  it("should create vinyl auction", () => {});
+  const ownerId = "owner-1";
+  let auction: Auction;
 
-  it("should place bid for an auction", () => {});
+  beforeEach(() => {
+    auction = new Auction(ownerId);
+  });
 
-  it("should finish an action", () => {});
+  it("should create vinyl auction", () => {
+    expect(auction).toBeInstanceOf(Auction);
+  });
+
+  it("should place bid for an auction", () => {
+    auction.placeBid(new Bid("bid-1", 100, "user-1"));
+
+    expect(auction.getCurrentBid()).toBe(100);
+  });
+
+  it("should place multiple bids for an auction", () => {
+    auction.placeBid(new Bid("bid-1", 100, "user-1"));
+    auction.placeBid(new Bid("bid-2", 150, "user-2"));
+
+    expect(auction.getCurrentBid()).toBe(150);
+  });
+
+  it("should not allow bid lower than current bid", () => {
+    auction.placeBid(new Bid("bid-1", 100, "user-1"));
+
+    expect(() => {
+      auction.placeBid(new Bid("bid-2", 50, "user-2"));
+    }).toThrow("Bid amount must be higher than the current bid.");
+  });
+
+  it("should not allow bid after auction end date", () => {
+    const pastDate = new Date();
+    pastDate.setDate(pastDate.getDate() - 1);
+
+    const auction = new Auction(ownerId, pastDate);
+
+    expect(() => {
+      auction.placeBid(new Bid("bid-1", 100, "user-1"));
+    }).toThrow("Auction has already ended.");
+  });
+
+  it("should not allow bid on own auction", () => {
+    auction.setOwnerId("seller-1");
+
+    const sellerId = "seller-1";
+    const bid = new Bid("bid-1", 100, sellerId);
+
+    try {
+      auction.placeBid(bid);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error?.message).toBe("Seller cannot bid on their own auction");
+    }
+  });
+
+  it("should finish an auction", () => {});
 });
--- test/vinyl-collection.test.ts
+++ test/vinyl-collection.test.ts
@@ -24,6 +24,9 @@ class VinylCollection {
   private vinyls: Vinyl[] = [];
 
   addVinyl(vinyl: Vinyl) {
+    if (this.getVinyl(vinyl)) {
+      throw new Error("Vinyl already exists in the collection");
+    }
     this.vinyls.push(vinyl);
   }
 
@@ -57,5 +60,22 @@ describe("Vinyl Collection", () => {
     expect(addedVinyl).toBeDefined();
     expect(vinylCollection.getVinyls()).toHaveLength(1);
     expect(vinylCollection.getVinyls()).toContain(addedVinyl);
+  });
+
+  it("should not add a vinyl with the same ID", () => {
+    const vinylCollection = new VinylCollection();
+    const id = crypto.randomUUID();
+    const title = "Bohemian Rhapsody";
+    const vinyl = new Vinyl(id, title);
+    vinylCollection.addVinyl(vinyl);
+
+    const vinyl2 = new Vinyl(id, title);
+    try {
+      vinylCollection.addVinyl(vinyl2);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error?.message).toBe("Vinyl already exists in the collection");
+    }
+    expect(vinylCollection.getVinyls()).toHaveLength(1);
   });
 });
